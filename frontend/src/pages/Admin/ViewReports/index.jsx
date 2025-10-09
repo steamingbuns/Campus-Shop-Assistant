edfb10553b52488201898759d8fbbba1c36d519b
NT777 <xnt777x@gmail.com>
2025-10-09 16:13:27 +0700

    Added report status change

--- frontend/src/pages/Admin/ViewReports/index.jsx
+++ frontend/src/pages/Admin/ViewReports/index.jsx
@@ -1,16 +1,10 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import './ViewReports.css'
 
 function ViewReports() {
     const [searchQuery, setSearchQuery] = useState('');
 
-    const status = useMemo(() =>[
-        {type: 'open', name: 'Open'},
-        {type: 'resolved', name: 'Resolved'},
-        {type: 'in_review', name: 'In Review'}
-    ])
-
-    const reportsList = useMemo(() => [
+    const [reportsList, setReportsList] = useState(() => [
         {
             id: 1,
             reporterUserID: 2,
@@ -55,6 +49,79 @@ function ViewReports() {
         }
     ])
 
+    const updateReportStatus = useCallback((reportId, newStatus) => {
+        setReportsList(currentReports => 
+            currentReports.map(report => 
+                report.id === reportId ? {...report, status: newStatus} : report
+            )
+        );
+    }, [])
+
+    const handleStatusChange = (reportId, currStatus) => {
+        let newStatus;
+        switch (currStatus) {
+            case 'open':
+                newStatus = 'in-review';
+                break;
+            case 'in-review':
+                newStatus = 'resolved';
+                break;
+            case 'resolved':
+                newStatus = 'in-review';
+                break;
+            default:
+                return;
+        }
+        updateReportStatus(reportId, newStatus);
+    }
+
+    const filteredReports = useMemo(() => {
+        if (!searchQuery) {
+            return reportsList;
+        }
+
+        const lowerCaseQuery = searchQuery.toLowerCase();
+
+        return reportsList.filter(report => {
+            const idMatch = report.id.toString().includes(lowerCaseQuery);
+            const detailsMatch = report.details.toLowerCase().includes(lowerCaseQuery);
+            const messageMatch = report.message && report.message.toLowerCase().includes(lowerCaseQuery);
+
+            return idMatch || detailsMatch || messageMatch;
+        })
+    })
+
+    const renderActionButton = (report) => {
+        switch (report.status) {
+            case 'open':
+                return (
+                    <button className='resolve-btn'
+                    onClick={() =>  handleStatusChange(report.id, 'open')}
+                    >
+                        Resolve
+                    </button>
+                )
+            case 'in-review':
+                return (
+                    <button className='mark-resolved-btn'
+                    onClick={() =>  handleStatusChange(report.id, 'in-review')}
+                    >
+                        Mark as Resolved
+                    </button>
+                )
+            case 'resolved':
+                return (
+                    <button className='reopen-btn'
+                    onClick={() =>  handleStatusChange(report.id, 'resolved')}
+                    >
+                        Re-open
+                    </button>
+                )
+            default:
+                return null;
+        }
+    }
+
     return (
         <div className='view-reports-page'>
             <h1>View Reports</h1>
@@ -69,33 +136,37 @@ function ViewReports() {
                 </div>
 
                 <div className='reports-list'>
-                    {reportsList.map(report => (
-                        <div key={report.id} className='report'>
-                            <div className='report-header'>
-                                <h3><strong>Report ID:</strong> {report.id}</h3>
-                                <h3><strong>Type:</strong> {report.type}</h3>
+                    {
+                        filteredReports.map(report => (
+                            <div key={report.id} className={`report status-${report.status}`}>
+                                <div className='report-header'>
+                                    <h3><strong>Report ID:</strong> {report.id}</h3>
+                                    <h3><strong>Status:</strong> <span className={`status-${report.status}`}>{report.status}</span></h3>
+                                </div>
+                                <div className='report-details'>
+                                    <p><strong>Type:</strong> {report.type}</p>
+                                    <p><strong>Reported by:</strong> {report.reporterUserID}</p>
+                                    {
+                                        report.type === 'product' ? (
+                                            <div className='product-details'>
+                                                <p><strong>Product ID:</strong> {report.productID}</p>
+                                                <p><strong>Details:</strong> {report.details}</p>
+                                            </div>
+                                        ) : (
+                                            <div className='message-details'>
+                                                <p><strong>Message:</strong> {report.message}</p>
+                                                <p><strong>Sent by:</strong> {report.fromUserID}</p>
+                                                <p><strong>Details:</strong> {report.details}</p>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                                <div className='report-actions'>
+                                    {renderActionButton(report)}
+                                </div>
                             </div>
-                            <p><strong>Reported by:</strong> {report.reporterUserID}</p>
-                            <div className='report-details'>
-                                {
-                                    report.type === 'product' ? (
-                                        // Render product report
-                                        <div className='product-details'>
-                                            <p><strong>Product ID:</strong> {report.productID}</p>
-                                            <p><strong>Details:</strong> {report.details}</p>
-                                        </div>
-                                    ) : (
-                                        <div className='message-details'>
-                                            <p><strong>Message:</strong> {report.message}</p>
-                                            <p><strong>Sent by:</strong> {report.fromUserID}</p>
-                                            <p><strong>Details:</strong> {report.details}</p>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                        </div>
-                    )
-                    )}
+                        ))
+                    }
                 </div>
             </div>
         </div>);
